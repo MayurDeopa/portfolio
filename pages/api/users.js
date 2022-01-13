40028569534ed56194ba8b91be2d023fe0759e24
MayurDeopa <91918025+MayurDeopa@users.noreply.github.com>
2022-01-14 01:08:03 +0530

    more bugs

--- pages/api/users.js
+++ pages/api/users.js
@@ -21,7 +21,7 @@ export default async function handler (req, res) {
           const user = await userEmails.create(req.body)
           res.status(201).send({ success: true, data: user })
         } catch (error) {
-          res.status(400).send({ success: false })
+          res.status(400).send({ success: false,err:error })
         }
         break
       default:
