96381f190e499ffd2f2c48aeb93f9f901623cee6
MayurDeopa <91918025+MayurDeopa@users.noreply.github.com>
2022-01-16 15:40:46 +0530

    success

--- pages/contact.js
+++ pages/contact.js
@@ -19,7 +19,7 @@ const Contact =()=>{
         setIsLoading(true)
         const res = await postData(contactInfo)
         setIsLoading(false)
-        setIsSubmitted(true)
+        setIsSubmitted(res.success)
     }
     return (
         <div className="page">
@@ -27,15 +27,15 @@ const Contact =()=>{
             <form className={styles.form}>
                 <section className={styles.sections}>
                     <p>Name</p>
-                    <input type='text' onChange={(e)=>setContactInfo({...contactInfo,name:e.target.value})} className={styles.input} required/>
+                    <input type='text' onChange={(e)=>setContactInfo({...contactInfo,name:e.target.value})} className={styles.input}/>
                 </section>
                 <section className={styles.sections}>
                     <p>Email</p>
-                    <input type='email' onChange={(e)=>setContactInfo({...contactInfo,email:e.target.value})} className={styles.input} required/>
+                    <input type='email' onChange={(e)=>setContactInfo({...contactInfo,email:e.target.value})} className={styles.input}/>
                 </section>
                 <section className={styles.sections}>
                     <p>Message</p>
-                    <textarea onChange={(e)=>setContactInfo({...contactInfo,message:e.target.value})} className={styles.message} required/>
+                    <textarea onChange={(e)=>setContactInfo({...contactInfo,message:e.target.value})} className={styles.message}/>
                 </section>
                 {isLoading?<MiniSpinner/>:<button  className={styles.submit} onClick={(e)=>submit(e)}>Submit</button>}
             </form>
