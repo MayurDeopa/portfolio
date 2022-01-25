710045d673137b0a728518f3a3f8b81058427b46
MayurDeopa <91918025+MayurDeopa@users.noreply.github.com>
2022-01-25 12:30:42 +0530

    Key Props

--- pages/projects.js
+++ pages/projects.js
@@ -5,6 +5,7 @@ import {DiReact,DiNodejsSmall,DiMongodb} from 'react-icons/di'
 const Projects =()=>{
     const projects = [
         {
+            id:1,
             title:"mi-store",
             description:"An E-Commerce Web-App built using React.js ,Node.js, Express,js and MongoDB ",
             image:'/mi-store-desktop.png',
@@ -13,6 +14,7 @@ const Projects =()=>{
             live:'https://blissful-dijkstra-1ee60d.netlify.app'
         },
         {
+            id:2,
             title:"Portfolio",
             description:"A portfolio website built using Next.js and hosted on Versel ",
             image:'/portfolio-desktop.png',
@@ -24,8 +26,8 @@ const Projects =()=>{
     return(
             <div className="page">
                 <h1>Projects</h1>
-                {projects.map((project,index)=>{
-                    return <Project key={index} details={project}/>
+                {projects.map((project)=>{
+                    return <Project key={project.id} details={project}/>
                 })}
             </div>
     )
