b115a235993045151362c9a9fe1f98934db005e6
MayurDeopa <frostkickerzvashi@gmail.com>
2022-02-18 22:01:37 +0530

    fixed image routes

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,4 @@
 import IndividualProject from '../components/IndividualProject'
-import portfolio from '../public/portfolio-desktop.png'
-import miStore from '../public/mi-store-desktop.png'
-
 
 const Projects =()=>{
     const projects = [
@@ -9,7 +6,7 @@ const Projects =()=>{
             id:1,
             title:"mi-store",
             description:"An E-Commerce Web-App built using React.js ,Node.js, Express,js and MongoDB ",
-            image:miStore,
+            image:'/mi-store-desktop.png',
             github:'https://github.com/MayurDeopa',
             live:'https://blissful-dijkstra-1ee60d.netlify.app',
         },
@@ -17,7 +14,7 @@ const Projects =()=>{
             id:2,
             title:"Portfolio",
             description:"A portfolio website built using Next.js and hosted on Versel ",
-            image:portfolio,
+            image:'/portfolio-desktop.png',
             github:'https://github.com/MayurDeopa',
             live:"https://portfolio-ten-eta-98.vercel.app",
         }
